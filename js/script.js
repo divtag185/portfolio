70e3596fc736ae3a45be96ebc61f4934375d252d
divtag185 <96375455+divtag185@users.noreply.github.com>
2022-08-25 21:32:57 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+
+
+//Sliding navigation code
+var nav = document.getElementById("nav");
+rollout_bars = document.getElementById("rollout_bars");
+rollin_bars = document.getElementById("rollin_bars");
+
+function rollOut() {
+    nav.style.marginLeft = "0";
+    rollout_bars.style.display="none";
+    rollin_bars.style.display="block";
+}
+
+function rollIn() {
+  if(screen.width < 991) {
+    nav.style.marginLeft = "-100%";
+    rollout_bars.style.display="block";
+    rollin_bars.style.display="none";
+  }
+}
+
+
+/*BACK TO TOP ANIMATION*/
+window.addEventListener('scroll', function(){
+  document.getElementById('back_to_top').classList.toggle("active", window.scrollY > 500);
+})
+
+/*LOADER*/
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut();
+
+
+
